32642f6c0c1bb74770de7ffbc50dbf33ea08546b
Rodrigo Tufiño <rtufino@ups.edu.ec>
2021-01-05 12:41:52 -0500

    Completado todas las operaciones

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -6,7 +6,32 @@ const _ = require('underscore');
 const app = express();
 
 app.get("/usuario", function (req, res) {
-  res.json("get Usuario");
+
+  let desde = req.query.desde || 0;
+  desde = Number(desde);
+
+  let limite = req.query.limite || 5;
+  limite = Number(limite);
+
+  Usuario.find({estado:true}, 'nombre email role estado google img')
+    .skip(desde)
+    .limit(limite)
+    .exec((err, usuarios) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
+        });
+      }
+
+      Usuario.count({estado:true}, (err, conteo) => {
+        res.json({
+          ok: true,
+          registros: conteo,
+          usuarios
+        });
+      });
+  });
 });
 
 app.post("/usuario", function (req, res) {
@@ -58,8 +83,38 @@ app.put("/usuario/:id", function (req, res) {
   });
 });
 
-app.delete("/usuario", function (req, res) {
-  res.json("delete Usuario");
+app.delete("/usuario/:id", function (req, res) {
+  let id = req.params.id;
+
+  let cambiarEstado = {
+    estado: false
+  }
+
+  // Usuario.findByIdAndDelete(id, (err, usuarioEliminado) => {
+  Usuario.findByIdAndUpdate(id, cambiarEstado, {nre: true, context:'query'}, (err, usuarioDB) => {
+    if (err){
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
+
+    if (!usuarioDB){
+      return res.status(400).json({
+        ok: false,
+        error: {
+          message: 'Usuario no encontrado en la BDD'
+        }
+      });
+    }
+
+    res.json({
+      ok: true,
+      usuario: usuarioDB
+    });
+
+  });
+
 });
 
 module.exports = app;
